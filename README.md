c0370a0895f6ce9ef7abbd45ec95716be967c7ce
lei <wanglei4687@gmail.com>
2022-02-23 21:54:01 +0800

    add emacs config

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# emacs_config
+# Emacs configuration
+
+## Module list
+- Themes and Modeline
+- Org mode
+- Treemacs
+- Undo tree
+- Ace windows
+- Which key
+- Ibuffer
+- Swiper/Ivy/Counsel
+- Better shell
+- Origami
+- Linum
+- Goto
+- Company
+- Flycheck
+- Yasnippet
+- Elpy
+- Go mode
+- Magit
+- Rust
+- Yaml mode
+- Web mode
+- Markdown mode
+- Auctex
+- Irony
+- Meghanada mode
+- Php mode
+- Docker
+
---
+++ init.el
@@ -0,0 +1,30 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+;; always show line number (first-line-number is 1)
+;; (global-linum-mode 1)
+
+;; Bootstrap `use-package`
+(unless
+  (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yaml org-bullets use-package monokai-pro-theme doom-themes doom-modeline color-theme-modern)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
