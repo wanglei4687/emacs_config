dacaa013286d30d50bc4b1c8eb266aa409c412eb
lei <wanglei4687@gmail.com>
2022-04-23 22:44:49 +0800

    update config

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ag yaml org-bullets use-package monokai-pro-theme doom-themes doom-modeline color-theme-modern))
+   '(treemacs-all-the-icons ag yaml org-bullets use-package monokai-pro-theme doom-themes doom-modeline color-theme-modern))
  '(use-package-always-ensure t)
  '(use-package-verbose nil))
 (custom-set-faces
