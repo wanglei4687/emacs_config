d272cf4c9a4e28a7fcf4212a4956163d61c710df
wanglei <wanglei4687@gmail.com>
2022-05-18 07:59:19 +0800

    add web development config

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(minions treemacs-all-the-icons ag yaml org-bullets use-package monokai-pro-theme doom-themes doom-modeline color-theme-modern))
+   '(typescript-mode minions treemacs-all-the-icons ag yaml org-bullets use-package monokai-pro-theme doom-themes doom-modeline color-theme-modern))
  '(use-package-always-ensure t)
  '(use-package-verbose nil))
 (custom-set-faces
