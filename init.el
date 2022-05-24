c57c141d45b88caa0a4a22d5f33656a7040cde92
wanglei <wanglei4687@gmail.com>
2022-05-24 23:16:40 +0800

    update config

--- init.el
+++ init.el
@@ -1,7 +1,4 @@
-(require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 ;; always show line number (first-line-number is 1)
@@ -15,18 +12,3 @@
 )
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(typescript-mode minions treemacs-all-the-icons ag yaml org-bullets use-package monokai-pro-theme doom-themes doom-modeline color-theme-modern))
- '(use-package-always-ensure t)
- '(use-package-verbose nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
